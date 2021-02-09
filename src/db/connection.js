c4ad7e0f16a4161a8d38d5afee5fbf67f3b925cc
FergusHenderson96 <FergusHenderson96@users.noreply.github.com>
2021-02-09 16:49:23 +0000

    added notes to code

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connection = async () => {
+    //async function which trys to connect to mongoose and will await connection before continuing with function 
     try {
         await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("successfully connected to MongoDB");
+        //logs to console if mongoose connects
     } catch (error) {
         console.log(error);
+        //if theres an error it will log 
     }
 };
 connection();
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,14 +1,21 @@
 const { Router } = require("express");
 const { getMyProfile, addUser, updateUserById, deleteUser, login, logout } = require("../controllers/users");
+//imports functions from users controller
 const userRouter = Router();
 //creates a mini version of the router 
 
 userRouter.get("/users/myProfile", getMyProfile);
+// requests profile through get request
 userRouter.post("/users", addUser);
+// adds user through post request
 userRouter.patch("/users/:id", updateUserById);
+// updates user via user id
 userRouter.delete("/users/:id", deleteUser);
+// deletes user via id
 userRouter.post("/users/login", login);
+// logs user in via post request
 userRouter.get("/users/logout", logout);
+// logs user out via get request
 
 module.exports = {
   userRouter,
